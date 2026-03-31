54f6c5363fbff54c46882c8738198bbce29b5472
umaisdev27 <umaisrashid125.com>
2026-03-31 23:42:16 +0500

    exception handling

---
+++ src/logger.py
@@ -0,0 +1,18 @@
+# any execution that happens, we should we able to log those info in some files
+# through tracking  
+
+import logging
+import os
+from datetime import datetime
+
+log_file = f'{datetime.now().strftime('%m_%d_%y_%H_%M_%S')}.log'
+logs_path = os.path.join(os.getcwd,"logs",log_file)
+os.makedirs(logs_path,exist_ok=True)
+
+log_file_path = os.path.join(logs_path, log_file)
+
+logging.basicConfig(
+    filename=log_file_path,
+    format= "[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO
+)
