518901e36f513387d1c4f70c120c9365418c15d6
umaisdev27 <umaisrashid125.com>
2026-04-01 00:21:44 +0500

    logger

--- src/logger.py
+++ src/logger.py
@@ -5,8 +5,8 @@ import logging
 import os
 from datetime import datetime
 
-log_file = f'{datetime.now().strftime('%m_%d_%y_%H_%M_%S')}.log'
-logs_path = os.path.join(os.getcwd,"logs",log_file)
+log_file = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path = os.path.join(os.getcwd(),"logs")
 os.makedirs(logs_path,exist_ok=True)
 
 log_file_path = os.path.join(logs_path, log_file)
@@ -16,3 +16,6 @@ logging.basicConfig(
     format= "[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO
 )
+
+if __name__=="__main__":
+    logging.info("we doing logging")
