029cea284321f5eefb249f258448ad49aee46b1d
umaisdev27 <umaisrashid125@gmail.com>
2026-04-05 00:03:22 +0500

    DataIngestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -9,6 +9,9 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
 # where i want to save the train,test and raw data are basically created in this class
 @dataclass
 class DataIngestionConfig:
@@ -48,4 +51,9 @@ class DataIngestion:
 #test run
 if __name__ == "__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data = obj.initiate_data_ingestion()
+
+    #now we will call DataTransformation
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
+
