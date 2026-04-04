5707e6d40b6efba9cf272b7a07a1e213a6c33d54
umaisdev27 <umaisrashid125@gmail.com>
2026-04-04 15:24:33 +0500

    utils

--- src/utils.py
+++ src/utils.py
@@ -1,2 +1,24 @@
 # reading the dataset from database
 # uploading project to the cloud
+# and have all the common things
+
+import os 
+import sys
+import dill #help to create the pkl file
+
+import numpy as np
+import pandas as pd
+
+from src.exception import ExceptionHandling
+
+def save_object(file_path,obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path,'wb') as file_obj:
+            dill.dump(obj, file_obj)
+
+    except Exception as e:
+        raise ExceptionHandling(e,sys)
