7be46a57091f7bcb425e4c5015eefedb00475359
umaisdev27 <umaisrashid125@gmail.com>
2026-04-05 01:07:58 +0500

    EvaluateFunction

--- src/utils.py
+++ src/utils.py
@@ -22,3 +22,26 @@ def save_object(file_path,obj):
 
     except Exception as e:
         raise ExceptionHandling(e,sys)
+    
+
+def evaluate_models(x_train,y_train,x_test, y_test,models):
+    try:
+        report = {}
+
+        for model_name, model in models.items(): #items provides both keys and values 
+            model = list(models.values())[i]
+
+            model.fit(x_train,y_train)
+
+            y_pred_train = model.predict(x_train)
+            y_pred_test = model.predict(x_test)
+
+            r2_score_train = r2_score(y_test,y_pred_train)
+            r2_score_test = r2_score(y_test,y_pred_test)
+
+            report[model_name] = r2_score_test
+
+            return report
+
+    except Exception as e:
+        raise ExceptionHandling(e,sys)
