733e313a1d6f18a3fe74eeb30cafbdf05bedf625
umaisdev27 <umaisrashid125@gmail.com>
2026-04-05 22:52:27 +0500

    ModelTraining

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -14,6 +14,7 @@ from sklearn.neighbors import KNeighborsRegressor
 from sklearn.linear_model import LinearRegression,Ridge,Lasso
 from sklearn.model_selection import RandomizedSearchCV
 from sklearn.svm import SVR
+from sklearn.metrics import r2_score
 
 from xgboost import XGBRegressor
 from catboost import CatBoostRegressor
@@ -26,7 +27,7 @@ class ModelTrain:
     def __init__(self):
         self.model_train_config = ModelTrainConfig()
 
-    def initiate_model_training(self,train_arr,  test_arr,preprocessor_obj_file):
+    def initiate_model_training(self,train_arr,  test_arr):
         try:
             logging.info("Splitting the training and testing Data")
 
@@ -52,6 +53,29 @@ class ModelTrain:
 
             model_report:dict=evaluate_models(x_train=x_train,x_test=x_test,
                                               y_train=y_train,y_test=y_test,models=models)
+            
+            #max will sort based on the values
+            #this just give the String
+            best_model_name = max(model_report, key=model_report.get)
+            best_model_score = model_report[best_model_name]
 
+            #it provides the actual model_name
+            best_model = models[best_model_name]
+
+            if best_model_score < 0.6:
+                raise ExceptionHandling('No best Model.. Found!!',sys)
+            
+            logging.info("The best Found model in both training and testing DataSet")
+
+            save_object(
+                file_path=self.model_train_config.trained_model_obj_file_path,
+                obj=best_model
+            )
+
+            predicted = best_model.predict(x_test)
+            r2_square = r2_score(y_test,predicted)
+
+            return r2_square
+        
         except Exception as e:
-            raise ExceptionHandling(e.sys)
+            raise ExceptionHandling(e,sys)
