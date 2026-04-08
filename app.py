85161aaa370049e78fc0f3c883bc824c184f9b3b
umaisdev27 <umaisrashid125@gmail.com>
2026-04-09 02:25:27 +0500

    Routes

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 
 from sklearn.preprocessing import StandardScaler
+from src.pipeline.predict_pipeline import CustomData, PredictPipeline
 
 app = Flask(__name__)
 
@@ -15,5 +16,23 @@ def predict_datapoint():
     if request.method == 'GET':
         return render_template('home.html') #home.html will have the simple input fields that will require for prediction
     else:
-        pass
+        data = CustomData(
+            gender = request.form.get('gender'),
+            lunch = request.form.get('lunch'),
+            race_ethnicity = request.form.get('race_ethnicity'),
+            parental_level_of_education = request.form.get('parental_level_of_education'),
+            test_preparation_score = request.form.get('test_preparation_score'),
+            reading_score = int(request.form.get('reading_score')),   
+            writing_score = int(request.form.get('writing_score'))    
+        )
+
+        pred_df = data.get_data_as_DataFrame(data)
+
+        predict_pipeline = PredictPipeline()
+        results = predict_pipeline.predict(pred_df)
+
+        return render_template('home.html',results=results[0])
     
+
+if __name__ == '__main__':
+    app.run('0.0.0.0',debug=True)
