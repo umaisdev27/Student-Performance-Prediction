dafd007087a1a030e47617bc190ba2c73ec29c0f
umaisdev27 <umaisrashid125@gmail.com>
2026-04-09 02:26:17 +0500

    MiddleMan

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -10,22 +10,34 @@ class PredictPipeline:
         pass
 
     def predict(self,features):
-        model_path = 'artifacts\model.pkl',
-        preprocessor_path = 'artifacts\preprocessor.pkl'
-        model = load_object(file_path = model_path )
+        try:
+            model_path = 'artifacts\model.pkl',
+            preprocessor_path = 'artifacts\preprocessor.pkl'
+            model = load_object(file_path = model_path )
+
+            #the data will go from preprocessor to the model
+            preprocessor = load_object(file_path=preprocessor_path)
+
+            data_scaled = preprocessor.transform(features)
+            preds = model.predict(data_scaled)
+            return preds
+        
+        except Exception as e:
+            raise ExceptionHandling(e,sys)
 
         
 #Responsible for mapping all the inputs we are giving to HTML to the BackEnd
+#CustomData is called First
 class CustomData:
     def __init__(self, gender:str, race_ethnicity:str,lunch:str, parental_level_of_education:str,
                  test_preparation_score:str, reading_score:int, writing_score:int):
         
-        self.gender = gender,
-        self.race_ethnicity = race_ethnicity,
-        self.lunch = lunch,
-        self.parental_level_of_education = parental_level_of_education,
-        self.test_preparation_score = test_preparation_score,
-        self.reading_score = reading_score,
+        self.gender = gender
+        self.race_ethnicity = race_ethnicity
+        self.lunch = lunch
+        self.parental_level_of_education = parental_level_of_education
+        self.test_preparation_score = test_preparation_score
+        self.reading_score = reading_score
         self.writing_score = writing_score
         # these values are coming from the home.html
 
