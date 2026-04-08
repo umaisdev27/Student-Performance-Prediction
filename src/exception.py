7e3c6e11d89cc2d0214be8b861570871402903b4
umaisdev27 <umaisrashid125@gmail.com>
2026-04-09 02:25:51 +0500

    ExceptionHandling

--- src/exception.py
+++ src/exception.py
@@ -12,7 +12,7 @@ def error_message_detail(error,error_detail:sys):
     return error_message
 
 class ExceptionHandling(Exception):
-    def __init__(self, error_message, error_detail:sys):
+    def __init__(self, error_message, error_detail):
         super().__init__(error_message)
         self.error_message = error_message_detail(error_message, error_detail=error_detail)
 
