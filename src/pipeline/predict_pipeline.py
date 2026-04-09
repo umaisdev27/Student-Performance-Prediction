eaedf8af819d2d43068f3fdcb203eff7f5825e51
umaisdev27 <umaisrashid125@gmail.com>
2026-04-10 02:21:32 +0500

    Prediction

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -11,8 +11,9 @@ class PredictPipeline:
 
     def predict(self,features):
         try:
-            model_path = 'artifacts\model.pkl',
-            preprocessor_path = 'artifacts\preprocessor.pkl'
+            model_path = 'artifacts/model.pkl'
+            preprocessor_path = 'artifacts/preprocessor.pkl'
+
             model = load_object(file_path = model_path )
 
             #the data will go from preprocessor to the model
@@ -30,30 +31,30 @@ class PredictPipeline:
 #CustomData is called First
 class CustomData:
     def __init__(self, gender:str, race_ethnicity:str,lunch:str, parental_level_of_education:str,
-                 test_preparation_score:str, reading_score:int, writing_score:int):
+                 test_preparation_course:str, reading_score:int, writing_score:int):
         
         self.gender = gender
         self.race_ethnicity = race_ethnicity
         self.lunch = lunch
         self.parental_level_of_education = parental_level_of_education
-        self.test_preparation_score = test_preparation_score
+        self.test_preparation_course = test_preparation_course
         self.reading_score = reading_score
         self.writing_score = writing_score
         # these values are coming from the home.html
 
     def get_data_as_DataFrame(self):
         try:
-            input_data = {
+            data = {
                 'gender': [self.gender],
                 'race_ethnicity':[self.race_ethnicity],
                 'lunch':[self.lunch],
                 'parental_level_of_education':[self.parental_level_of_education],
-                'test_preparation_score':[self.test_preparation_score],
+                'test_preparation_course':[self.test_preparation_course],
                 'reading_score':[self.reading_score],
                 'writing_score':[self.writing_score]
             }
 
-            return pd.DataFrame(input_data)
+            return pd.DataFrame(data)
         
         except Exception as e:
             raise ExceptionHandling(e,sys)
